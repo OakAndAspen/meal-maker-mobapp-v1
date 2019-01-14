6ab934bd833d22e6f4128f3a95c8d9a14afb36e1
Irina Despot <irinadespot@gmail.com>
2019-01-14 13:31:50 +0100

    MyGroups and NewGroup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import {SplashScreen} from '@ionic-native/splash-screen';
 import {StatusBar} from '@ionic-native/status-bar';
 import {IonicStorageModule} from "@ionic/storage";
 import {Geolocation} from '@ionic-native/geolocation';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { Camera } from '@ionic-native/camera';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {Camera} from '@ionic-native/camera';
 
 /* --- PROJECT SOURCES --- */
 import {MyApp} from './app.component';
@@ -17,6 +17,7 @@ import {LoginPage} from "../pages/login/login";
 import {MyGroupsPage} from '../pages/my-groups/my-groups';
 import {MyMealsPage} from '../pages/my-meals/my-meals';
 import {MyRecipesPage} from '../pages/my-recipes/my-recipes';
+import {NewGroupPage} from '../pages/new-group/new-group';
 import {MealDetailsPage} from '../pages/meal-details/meal-details';
 import {AuthProvider} from '../providers/auth/auth';
 import {AuthInterceptorProvider} from '../providers/auth-interceptor/auth-interceptor';
@@ -28,6 +29,7 @@ import {AuthInterceptorProvider} from '../providers/auth-interceptor/auth-interc
         MyGroupsPage,
         MyMealsPage,
         MyRecipesPage,
+        NewGroupPage,
         LoginPage,
         MealDetailsPage
     ],
@@ -45,6 +47,7 @@ import {AuthInterceptorProvider} from '../providers/auth-interceptor/auth-interc
         MyGroupsPage,
         MyMealsPage,
         MyRecipesPage,
+        NewGroupPage,
         LoginPage,
         MealDetailsPage
     ],
--- src/pages/my-groups/my-groups.ts
+++ src/pages/my-groups/my-groups.ts
@@ -1,13 +1,9 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {Camera, CameraOptions} from '@ionic-native/camera';
-
-/**
- * Generated class for the MyGroupsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {NewGroupPage} from "../new-group/new-group";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import Config from '../../config.js';
 
 @Component({
     selector: 'page-my-groups',
@@ -16,29 +12,35 @@ import {Camera, CameraOptions} from '@ionic-native/camera';
 export class MyGroupsPage {
 
     pictureData: string;
+    groups: Object[];
 
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
-        private camera: Camera) {
+        private camera: Camera,
+        private http: HttpClient) {
+        this.groups = [];
     }
 
     ionViewDidLoad() {
-        console.log('ionViewDidLoad MyGroupsPage');
-    }
-
-    takePicture() {
-        const options: CameraOptions = {
-            quality: 100,
-            destinationType: this.camera.DestinationType.DATA_URL,
-            encodingType: this.camera.EncodingType.JPEG,
-            mediaType: this.camera.MediaType.PICTURE
-        };
-        this.camera.getPicture(options).then(pictureData => {
-            this.pictureData = pictureData;
-        }).catch(err => {
-            console.warn(`Could not take picture because: ${err.message}`);
+        console.log("Hello...");
+        this.getGroups().then(response => {
+            console.log("Response: ", response);
+            this.groups = response.groups;
         });
     }
 
+    getGroups() {
+        return new Promise(resolve => {
+            this.http.get(Config.apiUrl+'/groups').subscribe(data => {
+                resolve(data);
+            }, err => {
+                console.log(err);
+            });
+        });
+    }
+
+    goToNewGroup() {
+        this.navCtrl.push(NewGroupPage);
+    }
 }
