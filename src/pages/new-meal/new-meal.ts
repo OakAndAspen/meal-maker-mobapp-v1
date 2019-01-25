f83752a94bb5753e9e18d77a62576b4db3d1f205
Irina Despot <irinadespot@gmail.com>
2019-01-25 20:55:40 +0100

    Create meal

--- src/pages/new-meal/new-meal.ts
+++ src/pages/new-meal/new-meal.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
-import {DateTime, NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams} from 'ionic-angular';
 import {config} from "../../app/config";
 import {Group} from "../../models/group";
 import {HttpClient} from "@angular/common/http";
 import {Recipe} from "../../models/recipe";
+import {Geolocation} from '@ionic-native/geolocation';
+import {LatLng, latLng, Map, MapOptions, Marker, marker, tileLayer} from 'leaflet';
 
 @Component({
     selector: 'page-new-meal',
@@ -13,16 +15,36 @@ export class NewMealPage {
 
     groups: Group[];
     group: Group;
-    date: DateTime;
     recipes: Recipe[];
     recipe: Recipe;
+    date: string;
+    time: string;
+
+    mapOptions: MapOptions;
+    map: Map;
+    mapMarkers: Marker[];
 
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
-        private http: HttpClient
+        private http: HttpClient,
+        private geolocation: Geolocation
     ) {
+        const url = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        let defaultLoc = latLng(46.778186, 6.641524);
+        this.mapOptions = {
+            layers: [
+                tileLayer(url, {maxZoom: 18})
+            ],
+            zoom: 13,
+            center: defaultLoc
+        };
+        this.mapMarkers = [new Marker(defaultLoc)];
 
+        let tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        this.date = tomorrow.toISOString().slice(0, 10);
+        this.time = "19:00";
     }
 
     ionViewDidLoad() {
@@ -35,7 +57,6 @@ export class NewMealPage {
     }
 
     findRecipe() {
-        console.log(this.group);
         let url = config.apiUrl + '/recipes/filtered/match?groupId=' + this.group._id;
         for (let p of this.group.members) {
             url += "&participants=" + p;
@@ -44,6 +65,43 @@ export class NewMealPage {
         this.http.get(url).subscribe(data => {
             // @ts-ignore
             this.recipes = data.recipes;
+            this.recipe = this.recipes[0];
+        });
+    }
+
+    onMapReady(map: Map) {
+        this.map = map;
+
+        // Get the user's location
+        this.geolocation.getCurrentPosition().then(pos => {
+            let userLoc = latLng(pos.coords.latitude, pos.coords.longitude);
+            this.map.panTo(userLoc);
+            this.mapMarkers = [new Marker(userLoc)];
+        }).catch(err => {
+            console.warn(`Could not retrieve user position because: ${err.message}`);
+        });
+    }
+
+    onClick($event) {
+        this.map.panTo($event.latlng);
+        this.mapMarkers = [new Marker($event.latlng)];
+    }
+
+    saveMeal() {
+        let loc = this.mapMarkers[0].getLatLng();
+        let data = {
+            groupId: this.group._id,
+            recipeId: this.recipe._id,
+            date: this.date + "T" + this.time + ":00",
+            participants: this.group.members,
+            location: {
+                x: loc.lat,
+                y: loc.lng
+            }
+        };
+        console.log(data);
+        this.http.post(config.apiUrl + '/meals', data).subscribe(data => {
+            this.navCtrl.pop();
         });
     }
 }
