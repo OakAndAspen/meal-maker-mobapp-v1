83494b9d521fd8316656b1b491d2b894ec73dd4b
Irina Despot <irinadespot@gmail.com>
2019-01-25 16:37:37 +0100

    Add recipe and member to group

--- src/pages/group-details/group-details.ts
+++ src/pages/group-details/group-details.ts
@@ -3,6 +3,8 @@ import {NavController, NavParams} from 'ionic-angular';
 import {HttpClient} from "@angular/common/http";
 import {config} from "../../app/config";
 import {Group} from "../../models/group";
+import {User} from "../../models/user";
+import {Recipe} from "../../models/recipe";
 
 @Component({
     selector: 'page-group-details',
@@ -11,6 +13,11 @@ import {Group} from "../../models/group";
 export class GroupDetailsPage {
 
     group: Group;
+    mode: string;
+    newMember: User;
+    newRecipe: Recipe;
+    allUsers: User[];
+    allRecipes: Recipe[];
 
     constructor(
         public navCtrl: NavController,
@@ -18,6 +25,7 @@ export class GroupDetailsPage {
         public http: HttpClient
     ) {
         this.group = this.navParams.data.group;
+        this.mode = "list";
     }
 
     ionViewDidLoad() {
@@ -28,16 +36,58 @@ export class GroupDetailsPage {
 
         for (let m of members) {
             this.http.get(config.apiUrl + '/users/' + m).subscribe(data => {
-                // @ts-ignore
-                this.group.members.push(data);
+                this.group.members.push(<User>data);
             });
         }
 
         for (let r of recipes) {
             this.http.get(config.apiUrl + '/recipes/' + r).subscribe(data => {
-                // @ts-ignore
-                this.group.recipes.push(data);
+                this.group.recipes.push(<Recipe>data);
             });
         }
+    }
+
+    addMember() {
+        this.http.get(config.apiUrl + "/users").subscribe(data => {
+            // @ts-ignore
+            this.allUsers = data.users;
+            this.allUsers = this.allUsers.filter(u =>
+                !this.group.members.find(m => m._id === u._id));
+            this.mode = "addMember";
+        });
+    }
+
+    addRecipe() {
+        this.http.get(config.apiUrl + "/recipes").subscribe(data => {
+            // @ts-ignore
+            this.allRecipes = data.recipes;
+            this.allRecipes = this.allRecipes.filter(u =>
+                !this.group.recipes.find(r => r._id === u._id));
+            this.mode = "addRecipe";
+        });
+    }
+
+    addUserToGroup() {
+        this.group.members.push(this.newMember);
+        let data = {
+            members: this.group.members.map(m => m._id)
+        };
+        this.http.patch(config.apiUrl + "/groups/" + this.group._id, data).subscribe(data => {
+            this.mode = 'list';
+        });
+    }
+
+    addRecipeToGroup() {
+        this.group.recipes.push(this.newRecipe);
+        let data = {
+            recipes: this.group.recipes.map(r => r._id)
+        };
+        this.http.patch(config.apiUrl + "/groups/" + this.group._id, data).subscribe(data => {
+            this.mode = 'list';
+        });
+    }
+
+    cancel() {
+        this.mode = 'list';
     }
 }
