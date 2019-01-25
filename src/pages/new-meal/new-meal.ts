d60c4fd22f5087c3bc07ac0834b5dab016235942
Irina Despot <irinadespot@gmail.com>
2019-01-25 14:43:28 +0100

    NewMeal

--- src/pages/new-meal/new-meal.ts
+++ src/pages/new-meal/new-meal.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
-import {Camera, CameraOptions} from '@ionic-native/camera';
+import {DateTime, NavController, NavParams} from 'ionic-angular';
+import {config} from "../../app/config";
+import {Group} from "../../models/group";
+import {HttpClient} from "@angular/common/http";
+import {Recipe} from "../../models/recipe";
 
 @Component({
     selector: 'page-new-meal',
@@ -8,9 +11,39 @@ import {Camera, CameraOptions} from '@ionic-native/camera';
 })
 export class NewMealPage {
 
+    groups: Group[];
+    group: Group;
+    date: DateTime;
+    recipes: Recipe[];
+    recipe: Recipe;
+
     constructor(
         public navCtrl: NavController,
-        public navParams: NavParams) {
+        public navParams: NavParams,
+        private http: HttpClient
+    ) {
+
     }
 
+    ionViewDidLoad() {
+        this.http.get(config.apiUrl + '/groups').subscribe(data => {
+            // @ts-ignore
+            this.groups = data.groups;
+            // @ts-ignore
+            this.group = this.groups[0];
+        });
+    }
+
+    findRecipe() {
+        console.log(this.group);
+        let url = config.apiUrl + '/recipes/filtered/match?groupId=' + this.group._id;
+        for (let p of this.group.members) {
+            url += "&participants=" + p;
+        }
+
+        this.http.get(url).subscribe(data => {
+            // @ts-ignore
+            this.recipes = data.recipes;
+        });
+    }
 }
